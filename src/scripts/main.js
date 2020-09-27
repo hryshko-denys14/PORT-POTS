cf98b14740d84955743c60580d124aff2a6524bf
Denys <gryshko_denys@gmail.com>
2020-09-27 14:07:09 +0300

    refuctor first 2 sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+//header
+
+const burgerButton = document.querySelector('.header__burger-btn');
+const mobileMenu = document.querySelector('.header__mob-menu');
+const closeButton = document.querySelector('.mob-menu__close-btn');
+
+burgerButton.onclick = () => {
+  mobileMenu.style.transform = 'translateX(0)';
+};
+
+closeButton.onclick = () => {
+  mobileMenu.style.transform = 'translateX(500%)';
+};
