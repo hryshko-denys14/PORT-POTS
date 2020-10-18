fc7cc6df2ec21e07bac9f88a14108048bca502a5
Denys Hryshko <denyshryshko@Denyss-MacBook-Pro.local>
2020-10-18 21:54:43 +0300

    materials desctop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,26 @@ const burgerButton = document.querySelector('.header__burger-btn');
 const mobileMenu = document.querySelector('.header__mob-menu');
 const closeButton = document.querySelector('.mob-menu__close-btn');
 
+const materialsButton = document.querySelector('.materials__circle');
+const materialsClose = document.querySelector('.materials__close');
+const materialsText = document.querySelector('.materials__hidden-text');
+
 burgerButton.onclick = () => {
   mobileMenu.style.transform = 'translateX(0)';
 };
 
 closeButton.onclick = () => {
   mobileMenu.style.transform = 'translateX(500%)';
+};
+
+materialsButton.onclick = () => {
+  materialsText.style.display = 'block';
+  materialsClose.style.display = 'block';
+  materialsButton.style.display = 'none';
+};
+
+materialsClose.onclick = () => {
+  materialsText.style.display = 'none';
+  materialsClose.style.display = 'none';
+  materialsButton.style.display = 'block';
 };
