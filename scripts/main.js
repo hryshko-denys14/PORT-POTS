d961b9af9ed633cd8d41c858fdaaeb44fe9aabd7
Denys Hryshko <gryshko.denys@gmail.com>
2021-01-29 21:15:52 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,82 +1,56 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-shadow */
 /* eslint-disable max-len */
-'use strict';
+import * as mobMobile from './mob-mobile.js';
+import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js';
 
-let isMobileMenuOpen = false;
+const swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
 
-function closeMobileMenu() {
-  mobileMenu.style.transform = 'translateX(500%)';
-  isMobileMenuOpen = false;
-}
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
 
-function handleOutsideClick(event) {
-  const classList = event.target.classList;
-
-  if (isMobileMenuOpen && !classList.contains('header__mob-menu')
-        && !classList.contains('header__burger-btn')) {
-    closeMobileMenu();
-  }
-}
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
 
 const burgerButton = document.querySelector('.header__burger-btn');
 const mobileMenu = document.querySelector('.header__mob-menu');
 const closeButton = document.querySelector('.mob-menu__close-btn');
-const mobileMenuButtons = document.getElementsByClassName('mob-menu__link');
+const mobileMenuButtons = document.querySelectorAll('.mob-menu__link');
 
-const materialsButtons = document.getElementsByClassName('materials__circle');
-const materialsCloses = document.getElementsByClassName('materials__close');
-const materialsTexts = document.getElementsByClassName('materials__hidden-text');
+const materialsButtons = document.querySelectorAll('.materials__circle');
+const materialsCloses = document.querySelectorAll('.materials__close');
+const materialsTexts = document.querySelectorAll('.materials__hidden-text');
 
-burgerButton.onclick = () => {
-  mobileMenu.style.transform = 'translateX(0)';
-  isMobileMenuOpen = true;
-};
+// mobile
 
-closeButton.onclick = closeMobileMenu();
+burgerButton.addEventListener('click', () => mobMobile.openMobileMenu(mobileMenu));
 
-for (let i = 0; i < mobileMenuButtons.length; i++) {
-  mobileMenuButtons[i].onclick = () => {
-    closeMobileMenu();
-  };
-}
+closeButton.addEventListener('click', () => mobMobile.closeMobileMenu(mobileMenu));
 
-document.addEventListener('click', handleOutsideClick);
+mobileMenuButtons.forEach(mobButton => mobButton.addEventListener('click', () => mobMobile.closeMobileMenu(mobileMenu)));
 
-for (let i = 0; i < materialsButtons.length; i++) {
-  materialsButtons[i].onclick = () => {
-    materialsTexts[i].style.display = 'block';
-    materialsCloses[i].style.display = 'block';
-    materialsButtons[i].style.display = 'none';
-  };
-}
+document.addEventListener('click', (ev) => mobMobile.handleOutsideClick(ev, mobileMenu));
 
-for (let i = 0; i < materialsCloses.length; i++) {
-  materialsCloses[i].onclick = () => {
-    materialsTexts[i].style.display = 'none';
-    materialsCloses[i].style.display = 'none';
-    materialsButtons[i].style.display = 'block';
-  };
-}
+// materials
+materialsButtons.forEach((materialsButton, index) => {
+  materialsButton.addEventListener('click', () => {
+    materialsTexts[index].style.display = 'block';
+    materialsCloses[index].style.display = 'block';
+    materialsButtons[index].style.display = 'none';
+  });
+});
 
-// slider
-const text = {
-  first: 'We have chosen Bobbiny recycled cotton cord to use as our draw string and wicking cord. Not only is it 100% recycled, it looks great and works really well at wicking water. ',
-  second: 'second second second second second second second second second second second second second second second second second second second second second second second second',
-  third: 'third third third third third third third third third third third third third third third third third third third third third third third third third third third third third',
-  fourth: 'fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth fourth',
-  fifth: 'test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11 test11',
-};
-
-const sliderButtons = document.getElementsByClassName('slider__btn');
-const textContent = document.querySelector('.slider__text');
-
-// eslint-disable-next-line no-unused-vars
-function changeText(id) {
-  for (let i = 0; i < sliderButtons.length; i++) {
-    sliderButtons[i].id === id
-      ? sliderButtons[i].classList.add('slider__btn--active')
-      : sliderButtons[i].classList.remove('slider__btn--active');
-
-    textContent.innerText = text[id];
-  }
-}
+materialsCloses.forEach((materialsClose, index) => {
+  materialsClose.addEventListener('click', () => {
+    materialsTexts[index].style.display = 'none';
+    materialsCloses[index].style.display = 'none';
+    materialsButtons[index].style.display = 'block';
+  });
+});
